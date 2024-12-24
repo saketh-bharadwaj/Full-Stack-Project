c4cd21fba1ba1fd48013217c46bd4934d6c69af7
Saketh Bharadwaj <saketh214@outlook.com>
2024-12-24 21:04:53 +0530

    Added delivery status updates

--- routes/vendor/vendorOperations.js
+++ routes/vendor/vendorOperations.js
@@ -5,6 +5,7 @@ import upload from '../../middlewares/multer.js';
 import vendorAuth from '../../middlewares/vendorAuth.js';
 import { ProductModel, ProductQuantityModel, ProductQuestionModel, ProductSalesModel } from '../../models/productModel.js';
 import { VendorInfoModel, VendorSalesModel } from '../../models/vendorModel.js';
+import { UserOrderHistoryModel } from '../../models/userModel.js';
 
 import { ProductReviewModel } from '../../models/productModel.js';
 import mongoose from 'mongoose';
@@ -572,6 +573,7 @@ router.post('/updateDeliveryStatus', vendorAuth, async function(req,res){
             vendorId: vendorId
         })
         const sale = vendorSales.sales.find(s => s.orderId == orderId && s.LineId == LineId);
+        const custId = sale.custId;
         if (sale.deliveryStatusCode === 0) {
             sale.deliveryStatusCode = 1;
             sale.deliveryStatus = "Order Accepted";
@@ -600,9 +602,65 @@ router.post('/updateDeliveryStatus', vendorAuth, async function(req,res){
         const adminOrder = await OrderModel.findOne({
             orderId: orderId
         })
-        let LineIdorder = adminOrder.orderItems.find(l => l.LineId==Lineid)
+        let LineIdorder = adminOrder.orderItems.find(l => l.LineId==LineId)
+
+        if (LineIdorder.deliveryStatusCode === 0) {
+            LineIdorder.deliveryStatusCode = 1;
+            LineIdorder.deliveryStatus = "Order Accepted";
+        } else if (LineIdorder.deliveryStatusCode === 1) {
+            LineIdorder.deliveryStatusCode = 2;
+            LineIdorder.deliveryStatus = "Order Dispatched";
+        } else if (LineIdorder.deliveryStatusCode === 2) {
+            LineIdorder.deliveryStatusCode = 3;
+            LineIdorder.deliveryStatus = "Out for Delivery";
+        } else if (LineIdorder.deliveryStatusCode === 3) {
+            LineIdorder.orderStatusCode = 1;
+            LineIdorder.orderStatus = "completed"
+            LineIdorder.isDelivered = true
+            LineIdorder.deliveryDateTime = deldatetime
+            LineIdorder.deliveryStatusCode = 4;
+            LineIdorder.deliveryStatus = "Delivery Successful";
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid delivery status code"
+            });
+        }
+        adminOrder.markModified('orderItems')
+        await adminOrder.save();
+
+        const custOrder = await UserOrderHistoryModel.findOne({
+            userID: custId
+        })
         
-        
+        const specificOrder = custOrder.orderHistory.find(order => order.orderId == orderId);
+        const specificLineItem = specificOrder.orderItems.find(item => item.LineId == LineId);
+        if (specificLineItem.deliveryStatusCode === 0) {
+            specificLineItem.deliveryStatusCode = 1;
+            specificLineItem.deliveryStatus = "Order Accepted";
+        } else if (specificLineItem.deliveryStatusCode === 1) {
+            specificLineItem.deliveryStatusCode = 2;
+            specificLineItem.deliveryStatus = "Order Dispatched";
+        } else if (specificLineItem.deliveryStatusCode === 2) {
+            specificLineItem.deliveryStatusCode = 3;
+            specificLineItem.deliveryStatus = "Out for Delivery";
+        } else if (specificLineItem.deliveryStatusCode === 3) {
+            specificLineItem.orderStatusCode = 1;
+            specificLineItem.orderStatus = "completed"
+            specificLineItem.isDelivered = true
+            specificLineItem.deliveryDateTime = deldatetime
+            specificLineItem.deliveryStatusCode = 4;
+            specificLineItem.deliveryStatus = "Delivery Successful";
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid delivery status code"
+            });
+        }
+
+        custOrder.markModified('orderHistory');
+        await custOrder.save();
+
         res.status(200).json({
             success: true,
             message: "Status updated Succesfully"
